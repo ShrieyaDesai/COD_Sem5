19921616802665dfe3f5acc06bc18bfca180354c
Shrieya D R <124590060+ShrieyaDesai@users.noreply.github.com>
2024-09-23 18:00:02 +0530

    Update bubble_sort_ascending.s

--- week4/bubble_sort_ascending.s
+++ week4/bubble_sort_ascending.s
@@ -1,15 +1,62 @@
 # write assembly code to arrange given array in ascending order using bubble sort algorithm
+    .data
+a:  .word 5, 3, 8, 2, 1     # Array of elements
+n:  .word 5                 # Number of elements in the array
 
-# if length of array = 5 ; no.of iterations = 5
-# no.of comparisons in first iteration = n-1 = 5-1
-# no.of comparisons in second iteration = n-2 = 5-2
+    .text
 
-.data 
-a: .word 5,3,8,2,1
-n: .word 5
-.text
-la x10,a
-lw x11,n
+    la   x10, a  
+    lw   x11, n             # Load the size of the array (n) into x11
 
-addi x12,x0,0 #i==0
-addi x
+    addi x12, x0, 0         # i = 0 (Outer loop index)
+    addi x16, x11, -1       # n-1 (loop bound for i) in x16
+
+
+    addi x13, x0, 0          # j = 0 (reset inner loop index)
+
+    
+    
+    #x11 = n
+    #x12 = i
+    #x13 = j
+    #x14 = a[j]
+    #x15 = a[j+1]
+    #x16 = n-1
+    #x17 = n-1-i
+    
+    
+loopi:
+    bge  x12, x16, end      # If i >= n-1, exit outer loop
+    sub  x17, x16, x12       # Compute (n-1-i) for inner loop bound
+
+loopj:
+    bge  x13, x17, next_i    # If j >= (n-1-i), go to the next iteration of outer loop
+
+    # Load a[j] into x14
+    slli x30, x13, 2         # j * 4 (word size)
+    add  x31, x10, x30       # Address of a[j]
+    lw   x14, 0(x31)         # Load a[j] into x14
+
+    # Load a[j+1] into x15
+    addi x29, x13, 1         # j + 1
+    slli x19, x29, 2         # (j+1) * 4 (word size)
+    add  x28, x10, x19       # Address of a[j+1]
+    lw   x15, 0(x28)         # Load a[j+1] into x15
+
+    # Compare a[j] and a[j+1]
+    blt  x14, x15, skip_swap # If a[j] < a[j+1], skip swap
+
+    # Swap a[j] and a[j+1]
+    sw   x15, 0(x31)         # Store a[j+1] in a[j]
+    sw   x14, 0(x28)         # Store a[j] in a[j+1]
+
+skip_swap:
+    addi x13, x13, 1         # j = j + 1
+    blt  x13, x17, loopj     # Repeat inner loop
+
+next_i:
+    addi x12, x12, 1         # i = i + 1
+    blt  x12, x16, loopi     # Repeat outer loop
+
+end:
+    li   x17, 10             # Load exit
