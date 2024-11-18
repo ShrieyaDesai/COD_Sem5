79545860d3afeb84c179487bf62e97a192b69f6f
ShrieyaDesai <desaishrieya@gmail.com>
2024-11-18 14:01:12 +0530

    modified week5 files

--- week5/c_pgm.s
+++ week5/c_pgm.s
@@ -1,31 +1,40 @@
+#Write an Assembly Program for the following C code:main() { unsigned short int a[11] = {0x1234, 0x5678, ...}; unsigned short int b[11] = {0x1234, 0x5678, ...}; unsigned short int c[11] = {0x1234, 0x5678, ...}; for(i = 0; i < 10; i++) { c[i] = a[i] * b[i] + c[i-1]; } } ```
+.data
+a: .half 0x1234, 0x5678, 0x9ABC, 0xDEF0, 0x1111, 0x2222, 0x3333, 0x4444, 0x5555, 0x6666, 0x7777
+b: .half 0x1234, 0x5678, 0x9ABC, 0xDEF0, 0x1111, 0x2222, 0x3333, 0x4444, 0x5555, 0x6666, 0x7777
+c: .half 0x1234, 0x5678, 0x9ABC, 0xDEF0, 0x1111, 0x2222, 0x3333, 0x4444, 0x5555, 0x6666, 0x7777
 
-    
-    .data
-a:.half 0x1234, 0x5678, 0x0012, 0x0123, 0x0020, 0x0086, 0x4007, 0x0430, 0x0011, 0x0120, 0x0032
-b:.half 0x1234, 0x5678, 0x0014, 0x0121, 0x0120, 0x0210, 0x0006, 0x0120, 0x0011, 0x0020, 0x0123
-c:.half 0x1234, 0x5678, 0x0255, 0x0024, 0x0050, 0x1220, 0x0440, 0x0060, 0x0002, 0x0000, 0x0002
 .text
-addi x10,x0,0
-la x11,a
-la x12,b
-la x13,c
-addi x14,x0,10
 
-func:
-    bge x10,x14,end #condition to end the looping
-    lh x15,0(x11)
-    lh x16,0(x12)
-    mul x17,x15,x16 #a*b
-    beqz x10,firstc # loop bcz it needs to compute c[0]
-    lh x18,-2(x13)   
-    add x17,x17,x18 #a*b+c[i-1]
+main:
+    la x5, a          # loading base address of array a,b,c
+    la x6, b          
+    la x7, c          
 
-firstc:
-    sh x17,0(x13)
-    addi x11,x11,1
-    addi x12,x12,1
-    addi x13,x13,1
-    addi x10,x10,1
-    j func
-end:
-    addi x20,x0,0xff #code complete
+    addi x19,x0,0         # i = 0 
+    addi x18,x0,10        # upper limit for the loop (10 iterations)
+
+    li x8, 0          # Initialize c[i-1] to 0 for the first iteration
+
+loop:
+    beq x19, x18, exit # Exit the loop when i == 10
+
+    lh x9, 0(x5)      # Load a[i] into x9
+    lh x10, 0(x6)     # Load b[i] into x10
+    mul x11, x9, x10  # x11 = a[i] * b[i]
+
+    add x11, x11, x8  # x11 = a[i] * b[i] + c[i-1]
+
+    sh x11, 0(x7)     # Store x11 into c[i]
+
+    mv x8, x11        # x8 = c[i] (becomes c[i-1] in the next iteration)
+
+    addi x5, x5, 2    # Increment address for a[i],b[i],c[i](2 bytes for .half)
+    addi x6, x6, 2    
+    addi x7, x7, 2 
+    addi x19, x19, 1  # i++
+
+    j loop            #loop starts again
+
+exit:
+    li x10, 10
